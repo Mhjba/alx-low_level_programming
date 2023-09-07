bcaae9c419928a17abd12c760a5afe1ae142e733
Mhjba <mahjoubaaitalla15@gmail.com>
2023-09-07 08:03:30 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * min - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int min(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
