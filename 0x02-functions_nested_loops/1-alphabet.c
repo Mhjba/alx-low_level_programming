a97c1da39caf870a44a15c0a49796617d101b634
Mhjba <mahjoubaaitalla15@gmail.com>
2023-09-12 05:32:51 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,12 @@
 #include "main.h"
-/*
+/**
  * main -Entry piont
  * description: print-alphabet
- * return 0
  */
 void print_alphabet(void)
 {
-	int ch;
-	for(ch = 'a'; ch <= 'z' ch++)
+	char ch;
+	for(ch = 'a'; ch <= 'z'; ch++)
 	_putchar (ch);
    _putchar('\n');
   
