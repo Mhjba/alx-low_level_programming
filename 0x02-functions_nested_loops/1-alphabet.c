f41f03e1f5aabd7a534bb8484b837ac535e3281c
Mhjba <mahjoubaaitalla15@gmail.com>
2023-09-12 05:29:14 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ * main -Entry piont
+ * description: print-alphabet
+ * return 0
+ */
+void print_alphabet(void)
+{
+	int ch;
+	for(ch = 'a'; ch <= 'z' ch++)
+	_putchar (ch);
+   _putchar('\n');
+  
+}
