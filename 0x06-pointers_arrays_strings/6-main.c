e8af0fff53a3b4331df515ce4fbe402be98006be
Mhjba <mahjoubaaitalla15@gmail.com>
2023-09-20 08:45:21 -0700

    array

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -8,3 +8,11 @@
  */
 int main(void)
 {
+	char str[] = "Prepare for the worst.\nhello-world 0123456\t.hello world\n";
+	char *ptr;
+
+	ptr = cap_string(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return (0);
+}
