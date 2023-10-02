7b23b0a21422615222b62c3430c96ea99d888de2
Mhjba <mahjoubaaitalla15@gmail.com>
2023-10-02 10:44:45 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,15 +2,13 @@
 
 /**
  * main -print name of program
- * @argc: int 
+ * @argc: int
  * @argv: string
  * return: 0
  */
 int main(int argc, char *argv[])
 {
-	if (argc > 0)
-		printf("%s\n", argv[0]);
-
+	printf("%s\n", argv[]);
 	return (0);
 
 }
