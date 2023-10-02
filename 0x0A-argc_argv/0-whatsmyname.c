0cb8a865c21b77336ffe02c3299f669bf2243a7c
Mhjba <mahjoubaaitalla15@gmail.com>
2023-10-02 10:16:10 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -print name of program
+ * @argc: 
+ * @argv:
+ * return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[]);
+	return (0);
+
+}
