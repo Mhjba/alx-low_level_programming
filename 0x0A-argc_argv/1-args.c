ecca5c140964a1c97a0ce538b659583b84b61b32
Mhjba <mahjoubaaitalla15@gmail.com>
2023-10-02 13:21:10 -0700

    args1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stidio.h>
+
+/**
+ * main -prints the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argc - 1);
+
+	return (0);
+}
