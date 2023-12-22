1718cc94fc1f9f820f2cc2f275cd903c3903b8e3
Mhjba <mahjoubaaitalla15@gmail.com>
2023-12-22 08:11:50 -0800

    hash

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table.
+ * @size: size of array .
+ *
+ * Return: a pointer to the newly created hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned int i = 0;
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+	return (ht);
+}
+
